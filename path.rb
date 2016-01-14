b0ddf7e4db865bc08af1a4af960cba659ec6f76a
ricky840 <ricky840@gmail.com>
2016-01-15 02:19:17 +0900

    too many changes

--- path.rb
+++ path.rb
@@ -6,42 +6,29 @@ require 'resolv'
 require 'ipaddr'
 require 'open3'
 require 'logger'
+require 'optparse'
 
-#Command locations in LSG
 GG = "/usr/local/akamai/tools/bin/ghost_grep"
 ESPRO = "/usr/local/akamai/tools/bin/es_pro"
 CURL = "/usr/bin/curl"
 NSH = "/usr/local/akamai/bin/nsh"
-
-#Location of image server log
 IMAGELOG = "/a/logs/web_tomcat/catalina.out"
+PRAGMA = "akamai-x-cache-on, akamai-x-get-request-id"
 
-#Set how many times it will try ghost_grep
 RETRY_GHOSTGREP = 10
+TIME_WINDOW = 300 #seconds
+RETRY_DELAY = 9 #seconds
+START_DELAY = 3 #seconds
 
-#Set how many times it will try grep logs from image server(yet implemented)
-RETRY_IMAGE_LOG = 3
-
-#Set number of log fields
 NUMBER_OF_FIELDS_F = 58
 NUMBER_OF_FIELDS_R = 75
 NUMBER_OF_FIELDS_S = 59
-
-#Time window(sec) for grep logs
-TIME_WINDOW = 300
 
 #Network (ff/essl) default is freeflow
 $server_network = "ff"
 
 #Hash that holds forward_machine/request_id
 $ip_and_reqid = Hash.new
-
-#logger
-$logger = Logger.new($stdout)
-$logger.formatter = proc do |severity, datetime, progname, msg|
-  #date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
-  puts "[#{severity}] #{msg}"
-end
 
 def akamai_domain?(domain)
   lookup = %x[dig #{domain} +short].split("\n")
@@ -51,17 +38,17 @@ def akamai_domain?(domain)
     end
   end
   puts "Hostname is not on Akamai"
-  exit
+  exit -1
 end
 
 def valid_url?(url)
   uri = URI.parse(URI.encode(url))
-  if not uri.kind_of?(URI::HTTP)
+  if not uri.kind_of? URI::HTTP
     puts "URL is not valid"
-    exit
+    exit -1
   end
 
-  if uri.kind_of?(URI::HTTPS)
+  if uri.kind_of? URI::HTTPS
     $server_network = "essl"
   end
 
@@ -72,9 +59,9 @@ def valid_url?(url)
   return uri
 end
 
-def prt_header(obj)
+def print_header(obj, direction)
+  puts "[#{direction}]"
   if obj.class == Net::HTTP::Get
-    puts "[Request Header]"
     puts "#{obj.method} #{obj.path}"
     obj.each_capitalized do |header, value|
       puts "#{header}: #{value}"
@@ -85,7 +72,6 @@ def prt_header(obj)
       puts header
     end
   else
-    puts "[Response Header]"
     puts "#{obj.code} #{obj.message}"
     obj.each_capitalized do |header, value|
       puts "#{header}: #{value}"
@@ -94,65 +80,62 @@ def prt_header(obj)
   end
 end
 
-def countdown(seconds, msg)
-  seconds.downto(0) do |index|
-    print "\r[INFO] #{msg} #{index}"
-    $stdout.flush
+def countDown(seconds, msg)
+  seconds.downto(1) do |sec|
+    $logger.info "#{msg} #{sec}"
     sleep 1
   end
-  puts "\n"
 end
 
-def grep_log_imageserver(reqid, server_ip)
+def runCommand(cmd)
+  Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+    stdin.close
+    if $?.exited?
+      result = stdout.read
+      if cmd.include? "curl" then result = stderr.read end
+      return result
+    elsif not $?.exitstatus == 0
+      $logger.warn "error occured while running command #{$?.exitstatus}"
+    end
+  end
+end
+
+def grepLogFromImageServer(reqid, server_ip)
   cmd = "#{NSH} #{server_ip} cat #{IMAGELOG} | grep #{reqid}"
 
   image_logs = String.new
   image_server_reqid = String.new
 
-  $logger.info "Pulling log to find image server request id"
-  Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-    stdin.close
-    if $?.exited?
-      output = stdout.read
-      case output.split("\n").size
-        when 0
-          $logger.info "Could not find any logs from image server."
-        else
-          $logger.info "Found log, validating.."
-          if output.split[7] == ":ghost-auth-data" #only need the one requested by ghost
-            auth_data = output.scan(/"([^"]*)"/).join.split
-            if auth_data[6].include? reqid
-              image_server_reqid = output.split[6].strip
-              $logger.info "Found the request id [#{output.split[6]}] from the image server."
-            end
-          end
-      end
+  $logger.info "pulling logs from the image server"
+
+  output = runCommand(cmd)
+  case output.split("\n").size
+    when 0
+      $logger.info "could not find any logs from image server"
     else
-      $logger.warn "There was an error with running command. Exitstatus: #{$?.exitstatus}"
-    end
-  end #open3 end
+      $logger.info "found log, validating.."
+      if output.split[7] == ":ghost-auth-data" #only need the one requested by ghost
+        auth_data = output.scan(/"([^"]*)"/).join.split
+        if auth_data[6].include? reqid
+          image_server_reqid = output.split[6].strip
+          $logger.info "found the request id [#{output.split[6]}]"
+        end
+      end
+  end
 
   if not image_server_reqid.empty?
-    cmd_greplog = "#{NSH} #{server_ip} cat #{IMAGELOG} | grep #{image_server_reqid}"
-
-    $logger.info "Pulling image server logs with request id [#{image_server_reqid}]"
-    Open3.popen3(cmd_greplog) do |stdin, stdout, stderr, wait_thr|
-      stdin.close
-      if $?.exited?
-        output = stdout.read
-        if not output.empty?
-          image_logs = output
-        end
-      else
-        $logger.warn "There was an error with running command. Exitstatus: #{$?.exitstatus}"
-      end
-    end #open3 end
+    cmd2= "#{NSH} #{server_ip} cat #{IMAGELOG} | grep #{image_server_reqid}"
+    $logger.info "getting image log set from image server with request id [#{image_server_reqid}]"
+    output2 = runCommand(cmd2)
+    if not output2.empty?
+      image_logs = output2
+    end
   end
 
   return image_logs
 end
 
-def find_forward_machine_from_imagelog(raw_logs)
+def findForwardMachineFromImageLog(raw_logs)
   forward_list = Array.new
 
   raw_logs.each_line do |line|
@@ -163,7 +146,7 @@ def find_forward_machine_from_imagelog(raw_logs)
         if each.split.last =~ Resolv::IPv4::Regex ? true : false
           first_edge_ipaddr = each.split.last
           first_request_id = fetch_info[6].split(".").reverse.first
-          forward_list.push(put_request_id(first_edge_ipaddr, first_request_id))
+          forward_list.push(putRequestId(first_edge_ipaddr, first_request_id))
 
           #we would only need the first Edge IP address and request ID.
           break
@@ -175,30 +158,17 @@ def find_forward_machine_from_imagelog(raw_logs)
   return forward_list
 end
 
-def ghost_grep(start_time, end_time, reqid, ipaddr, network)
+def ghostGrep(start_time, end_time, reqid, ipaddr, network)
   cmd = "#{GG} --#{network} --use-normandy --range=#{start_time}-#{end_time} #{reqid} #{ipaddr}"
-  # puts "Running #{cmd}"
-  $logger.info "Running ghost_grep on #{ipaddr} with request id #{reqid}"
 
   logs = Array.new
 
   RETRY_GHOSTGREP.times do |index|
-    $logger.info "#{index} attempt in grepping logs from #{ipaddr}"
+    $logger.info "#{index} attempt, running ghost_grep on #{ipaddr}. request id #{reqid}"
 
     #use %x to see status of ghost_grep
     #output = %x[#{cmd}]
-    output = String.new
-
-    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-      stdin.close
-      output = stdout.read
-      if $?.exited?
-        $logger.info "ghost_grep ran successfully with pid #{$?.pid}"
-      else
-        $logger.warn "It seems there was an issue with running ghost_grep. Exitstatus: #{$?.exitstatus}"
-        next
-      end
-    end
+    output = runCommand(cmd)
 
     output.each_line do |line|
       log_line = line.split
@@ -215,24 +185,23 @@ def ghost_grep(start_time, end_time, reqid, ipaddr, network)
     end
 
     if logs.length > 0
-      $logger.info "Pulled log successfully."
+      $logger.info "okay, we have logs"
       break
     elsif logs.length == 0
-      $logger.info "No log was found"
-      countdown(9, "Retry in")
+      $logger.info "oops, could not find any logs"
+      countDown(RETRY_DELAY, "will retry in")
     end
 
     if index == RETRY_GHOSTGREP - 1
-      $logger.warn "Could not find any logs from #{ipaddr}. You might want to try manaully with request id #{reqid}"
+      $logger.warn "failed to logs from #{ipaddr}. might try manaully with request id: #{reqid}"
     end
   end
 
   return logs
 end
 
-def find_forward_machine(arr_logs)
+def findForwardMachine(arr_logs)
 
-  #list of forward IP addresses
   forward_list = Array.new
 
   arr_logs.each do |log_line|
@@ -258,10 +227,9 @@ def find_forward_machine(arr_logs)
         #that is not the log we're looking for
         if not forward_err == "ERR_DNS_IN_REGION"
           forward_ipaddr = log_line.split[10]
-          forward_list.push(put_request_id("parent #{forward_ipaddr}", request_id))
+          forward_list.push(putRequestId("parent #{forward_ipaddr}", request_id))
 
-          #there might be more than one parent
-          next
+          next #there might be more than one parent
         end
       end
 
@@ -272,15 +240,15 @@ def find_forward_machine(arr_logs)
           begin
             forward_icp = IPAddr.new(forward_hostname)
           rescue IPAddr::InvalidAddressError => error
-            $logger.warn "Request was forwarded to icp but forward hostname was not an IP address"
+            $logger.warn "request was forwarded to icp but forward hostname was not an IP address"
             next
           end
 
           first_octet = log_source_ip.split(".")[0]
           arr_forward_ipaddr = forward_hostname.split(".")
           arr_forward_ipaddr[0] = first_octet
-          forward_list.push(put_request_id("icp " + arr_forward_ipaddr.join("."), request_id))
-          $logger.info "The request was forwarded to ICP #{forward_icp} and real IP of the server is #{arr_forward_ipaddr.join(".")}"
+          forward_list.push(putRequestId("icp " + arr_forward_ipaddr.join("."), request_id))
+          $logger.info "forwarded to ICP #{forward_icp}. forward IP was changed to #{arr_forward_ipaddr.join(".")}"
 
           next
         end
@@ -288,7 +256,7 @@ def find_forward_machine(arr_logs)
 
       #if it was forwarded to image server
       if object_status =~ /o/ and forward_hostname.include?("mobile.akadns.net")
-        return forward_list.push("image_server #{put_request_id(log_line.split[10], request_id)}")
+        return forward_list.push("image_server #{putRequestId(log_line.split[10], request_id)}")
       end
     end
   end
@@ -296,17 +264,17 @@ def find_forward_machine(arr_logs)
   return forward_list
 end
 
-def get_request_id(ipaddr)
+def getRequestId(ipaddr)
   return $ip_and_reqid[ipaddr]
 end
 
-def put_request_id(ipaddr, reqid)
+def putRequestId(ipaddr, reqid)
   index = 1
 
   while true
     if not $ip_and_reqid.include? ipaddr
       $ip_and_reqid[ipaddr] = reqid
-      $logger.info "New request was found: #{ipaddr} - #{reqid} inserted."
+      $logger.info "new request: #{ipaddr} - #{reqid}"
       break
     elsif $ip_and_reqid.include? ipaddr
       ipaddr = ipaddr.split.last.split("_").first + "_" + index.to_s
@@ -317,7 +285,7 @@ def put_request_id(ipaddr, reqid)
   return ipaddr
 end
 
-def es_pro(forward)
+def espro(forward)
   ipaddr = forward.split.last.split("_").first
   output = %x[#{ESPRO} #{ipaddr}].strip
   if $?.exitstatus == 1
@@ -336,81 +304,133 @@ end
 
 if __FILE__ == $0
 
-  unless ARGV.length == 1
-    puts
-    puts "Usage:"
-    puts "   kurl.rb URL"
-    puts "   kurl.rb http://www.foo.com/apple.jpg"
-    puts
+  options = {}
+
+  optparse = OptionParser.new do |opts|
+    opts.banner = "Usage: kurl.rb [options] URL"
+
+    opts.on('-H', '--host HOST', 'Pass host header to server') do |host|
+      options[:host] = host
+    end
+
+    opts.on('-q', '--quiet', 'Silence output. Print only headers and logs') do
+      options[:quiet] = true
+    end
+
+    opts.on('-h', '--help', 'Display help') do
+      puts opts
+      exit
+    end
+  end
+
+  begin
+    optparse.parse!
+  rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+    puts optparse
     exit
   end
 
-  #Validate URL
+  unless ARGV.length == 1
+    puts optparse
+    exit
+  end
+
   url = ARGV[0].strip
   uri = valid_url?(url)
 
-  #Make sure the domain is on Akamai
-  akamai_domain?(uri.host)
+  if not uri.host =~ Resolv::IPv4::Regex ? true : false
+    akamai_domain?(uri.host)
+  end
+
+  output_redir = options[:quiet] ? nil : $stdout
+  $logger = Logger.new(output_redir)
+  $logger.formatter = proc do |severity, datetime, progname, msg|
+    puts "\e[0;36m#{severity.downcase}\e[0m #{msg}"
+  end
 
   case $server_network
     when 'ff'
       req = Net::HTTP::Get.new(uri.to_s)
-      req['Pragma'] = "akamai-x-cache-on, akamai-x-get-request-id, akamai-x-cache-remote-on"
+      req['Pragma'] = PRAGMA
+      if options[:host] then req['Host'] = options[:host] end
       http = Net::HTTP.new(uri.host, uri.port)
       res = http.request(req)
+
+      print_header(req, "Request Header")
+      print_header(res, "Response Header")
+
+      if not res['X-Akamai-Request-ID'] or not res['X-Cache']
+        $logger.warn "request ID or Edge IP does not exist"
+        exit
+      end
+
       request_id = res['X-Akamai-Request-ID'].split(".").reverse.first
       edge_ipaddr = %x[dig #{res['X-Cache'].split[2]} +short].strip
-      prt_header(req)
-      prt_header(res)
+
     when 'essl'
-      req_header = ["[Request Header]"]
-      res_header = ["[Response Header]"]
-      curl = "#{CURL} -v -k -o /dev/null #{uri.to_s} -H 'Pragma: akamai-x-cache-on, akamai-x-get-request-id'"
-      Open3.popen3(curl) do |stdin, stdout, stderr, wait_thr|
-        stdin.close
-        if $?.exited?
-          output = stderr.read #headers are considered to be debug informations
-          if not output.empty?
-            output.each_line do |line|
-              if line.start_with? ">"
-                req_header.push(line[1..line.length].strip)
-              elsif line.start_with? "<"
-                res_header.push(line[1..line.length].strip)
-              end
-            end
+      curl = "#{CURL} -v -k -o /dev/null '#{uri.to_s}' -H 'Pragma: #{PRAGMA}'"
+      if options[:host] then curl = curl + " -H 'Host: #{options[:host]}'" end
+
+      req = Array.new
+      res = Array.new
+
+      output = runCommand(curl)
+      if not output.empty?
+        output.each_line do |line|
+          if line.start_with? ">"
+            req.push(line[1..line.length].strip)
+          elsif line.start_with? "<"
+            res.push(line[1..line.length].strip)
           end
-        else
-          $logger.warn "There was an error with running command(curl). Exitstatus: #{$?.exitstatus}"
-          exit
+        end
+      else
+        $logger.warn "no response from curl command"
+        exit
+      end
+
+      print_header(req, "Request Header")
+      print_header(res, "Response Header")
+
+      request_id = String.new
+      edge_ipaddr = String.new
+
+      res.each do |res_header|
+        if res_header.split(":").first == "X-Akamai-Request-ID"
+          request_id = res_header.split(":").last.strip.split(".").reverse.first
+        elsif res_header.split(":").first == "X-Cache"
+          edge_hostname = res_header.split(":").last.strip.split[2]
+          edge_ipaddr = %x[dig #{edge_hostname} +short].strip
         end
       end
-      request_id = res_header.detect {|header| header.include? "X-Akamai-Request-ID"}.split(":").last.split(".").reverse.first.strip
-      edge_hostname = res_header.detect {|header| header.include? "X-Cache"}.split(":").last.split[2].strip
-      edge_ipaddr = %x[dig #{edge_hostname} +short].strip
-      prt_header(req_header)
-      prt_header(res_header)
+
+      if request_id.empty? or edge_ipaddr.empty?
+        $logger.warn "request ID or Edge IP does not exist"
+        exit
+      end
   end
 
   #Make a delay for logs to be ready
-  countdown(3, "Starts in")
+  countDown(START_DELAY, "starts in")
 
-  #Set time windown +-5 mins of current time
+  #grep window
   before_current_time = (Time.now.utc - TIME_WINDOW).strftime("%m/%d/%Y/%H:%M")
   after_current_time = (Time.now.utc + TIME_WINDOW).strftime("%m/%d/%Y/%H:%M")
 
-  #To save all logs
+  #entire logs
   entire_logs = Hash.new
 
-  #IP and reqID pair
-  put_request_id(edge_ipaddr, request_id)
+  #forward machine and reqID pair
+  putRequestId(edge_ipaddr, request_id)
 
   #forward list in order
   forward_list = [edge_ipaddr]
+
+  #interate list index
   forward_index = 0
 
   while true
     if forward_index == forward_list.length
-      $logger.info "Completed."
+      $logger.info "completed."
       break
     end
 
@@ -418,33 +438,34 @@ if __FILE__ == $0
     forward_server_ip = forward_next.split.last.split("_").first
 
     if forward_next.include? "image_server"
-      $logger.info "#{forward_next} was found."
-      image_logs = grep_log_imageserver(get_request_id(forward_next.split[1]), forward_server_ip)
+      $logger.info "found #{forward_next}"
+      image_logs = grepLogFromImageServer(getRequestId(forward_next.split[1]), forward_server_ip)
       entire_logs[forward_next] = image_logs
-      forward_ips = find_forward_machine_from_imagelog(image_logs)
+      forward_ips = findForwardMachineFromImageLog(image_logs)
     elsif forward_server_ip =~ Resolv::IPv4::Regex ? true : false
-      logs = ghost_grep(before_current_time, after_current_time, get_request_id(forward_next), forward_server_ip, $server_network)
+      logs = ghostGrep(before_current_time, after_current_time, getRequestId(forward_next), forward_server_ip, $server_network)
       entire_logs[forward_next] = logs
-      forward_ips = find_forward_machine(logs)
+      forward_ips = findForwardMachine(logs)
     end
 
     if forward_ips.length > 0
-      $logger.info "Request was forwarded to #{forward_ips.inspect}"
+      $logger.info "request was forwarded to #{forward_ips.inspect}"
       forward_list.concat(forward_ips)
-      $logger.info "Forward list updated. #{forward_list.inspect} and current index is #{forward_index}"
+      $logger.info "forward list updated #{forward_list.inspect}. current index #{forward_index}"
     end
 
     forward_index = forward_index.next
   end #while end
 
-  puts "[LOG]"
+  puts "\n"
   forward_list.each do |forward|
-    puts "\n[#{forward}] #{es_pro(forward)}"
-    if not entire_logs[forward] == nil
-      puts entire_logs[forward]
+    puts "[#{forward}] #{espro(forward)}\n"
+    if entire_logs[forward].empty?
+      puts "no log was found."
     else
-      puts "No log was found"
+      puts entire_logs[forward]
     end
+    puts "\n"
   end
 
-end
+end #end
